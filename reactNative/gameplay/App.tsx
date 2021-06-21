805b92cc4a7bc13d3d309228eeeec0edc5aab13e
adriisoliveira <adrianne.soliveira@hotmail.com>
2021-06-21 13:37:44 -0300

    React 1

--- reactNative/gameplay/App.tsx
+++ reactNative/gameplay/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
-import { SigIn } from "./src/screens/SignIn";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SigIn />
-  )
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
