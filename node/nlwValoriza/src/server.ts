e328924ed0a81cb830dd56885f5eaef56419d339
adriisoliveira <adrianne.soliveira@hotmail.com>
2021-06-23 18:20:31 -0300

    New changes

--- node/nlwValoriza/src/server.ts
+++ node/nlwValoriza/src/server.ts
@@ -1,24 +1,12 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
+
 // @types/express
 const app = express();
 
-/**
- * Get    => Buscar uma informação
- * Post   => Inserir (Criar) uma informação
- * Put    => Alterar uma informação
- * Delete => Remover um dado
- * Patch  => Alterar uma informação específica
- */
 
-app.get("/test", (request, response) => {
-  //Request   => Entrando
-  //Response  => Response
-  return response.send("Olá NLW")
-})
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW")
-})
 
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
